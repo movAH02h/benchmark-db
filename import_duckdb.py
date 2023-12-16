8d8b58c51020a329379561d5f2f1655a970523ad
movAH02h <122667404+movAH02h@users.noreply.github.com>
2023-12-16 14:23:47 +0300

    Delete import_duckdb.py

--- import_duckdb.py
+++
@@ -1,5 +0,0 @@
-import duckdb
-
-connection = duckdb.connect('database2.duckdb')
-cur = connection.cursor()
-cur.execute('''CREATE TABLE users AS SELECT * FROM read_csv_auto('D:\\Python\\nyc_yellow_big.csv')''')
